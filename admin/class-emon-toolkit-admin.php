6a23ecd2dcbc28e8929951e4dbeaa8b34206a0a7
emon4u <emon82240@gmail.com>
2021-06-05 21:49:18 +0600

    Boilerplate Files added

---
+++ admin/class-emon-toolkit-admin.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * @link       https://github.com/emon4u/
+ * @since      1.0.0
+ *
+ * @package    Emon_Toolkit
+ * @subpackage Emon_Toolkit/admin
+ */
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the admin-specific stylesheet and JavaScript.
+ *
+ * @package    Emon_Toolkit
+ * @subpackage Emon_Toolkit/admin
+ * @author     Emon Ahmed <emon82240@gmail.com>
+ */
+class Emon_Toolkit_Admin {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $plugin_name    The ID of this plugin.
+	 */
+	private $plugin_name;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $plugin_name       The name of this plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	public function __construct( $plugin_name, $version ) {
+
+		$this->plugin_name = $plugin_name;
+		$this->version = $version;
+
+	}
+
+	/**
+	 * Register the stylesheets for the admin area.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Emon_Toolkit_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Emon_Toolkit_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/emon-toolkit-admin.css', array(), $this->version, 'all' );
+
+	}
+
+	/**
+	 * Register the JavaScript for the admin area.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Emon_Toolkit_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Emon_Toolkit_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/emon-toolkit-admin.js', array( 'jquery' ), $this->version, false );
+
+	}
+
+}
---
+++ includes/class-emon-toolkit-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/emon4u/
+ * @since      1.0.0
+ *
+ * @package    Emon_Toolkit
+ * @subpackage Emon_Toolkit/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Emon_Toolkit
+ * @subpackage Emon_Toolkit/includes
+ * @author     Emon Ahmed <emon82240@gmail.com>
+ */
+class Emon_Toolkit_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
